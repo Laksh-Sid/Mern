0f0ce53677dd8e7f6c0534bc9f3ca6ec061f7497
Laksh-Sid <lakshana242003@gmail.com>
2024-01-09 09:50:31 +0530

    first commit

--- react/my-project/src/App.jsx
+++ react/my-project/src/App.jsx
@@ -8,14 +8,26 @@ function App() {
 
   return (
     <div className="container">
-      
+
+
       <div className="flex">
-      <div className="logo"style={{ color: 'white', fontSize: 27.11, fontFamily: 'Jost', fontWeight: '400', wordWrap: 'break-word', textdecoration: 'none' }}>TEA<b style={{ color: '#FB471F', fontSize: 40.67, fontFamily: 'Kanit', fontWeight: '600', wordWrap: 'break-word', textdecoration: 'none' }}>CAFE</b></div>
+        <div className="logo" style={{ color: 'black', fontSize: 27.11, fontFamily: 'Jost', fontWeight: '400', wordWrap: 'break-word', textdecoration: 'none' }}>Elite<b style={{ color: '#FB471F', fontSize: 40.67, fontFamily: 'Kanit', fontWeight: '600', wordWrap: 'break-word', textdecoration: 'none' }}>Trip</b></div>
         <div className="f1">Home</div>
-        <div className="f2">About</div>
-        <div className="f3">Contact</div>
+        <div className="f2">Guide</div>
+        <div className="f3">Places</div>
+        <div className="f4">Food</div>
+        <div className="f5">Sign In</div>
+      </div>
+
+      <div className="body">
+        <p id="p1">FRANCE</p>
+        <p id="p2">Paris,FRANCE</p>
+        <div className="b1">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat...<br /><br /><span>READ MORE</span></p>
+        </div>
       </div>
     </div>
+
   )
 }
 
